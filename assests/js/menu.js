4df7a5c9cbd31d5641ab481aa528b39ccac6e3b9
kauasilvandrade <kauaa422@gmail.com>
2025-03-26 14:32:49 -0300

    Ajustes

--- assests/js/menu.js
+++ assests/js/menu.js
@@ -5,4 +5,11 @@ const cabecalho = document.querySelector('.header');
 // EVENTOS
 hamburguer.addEventListener('click', function() {
     cabecalho.classList.toggle('ativo') ;
+    window.addEventListener('resize', () => cabecalho.classList.remove('ativo'))
+    const links = document.querySelectorAll('a')
+    links.forEach(link => {
+        link.addEventListener('click', () => cabecalho.classList.remove('ativo'))
+    })
+    console.log(links)
+    
 })
